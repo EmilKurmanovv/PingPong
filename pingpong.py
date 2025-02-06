cdd1f9e66602ef4c064947467dc678143d38e2ab
EmilKurmanovv <zenadzamala@gmail.com>
2025-02-06 18:48:29 +0300

    Add files via upload
    add ball

--- pingpong.py
+++ pingpong.py
@@ -5,11 +5,16 @@ window = pygame.display.set_mode((700,500))
 fps = pygame.time.Clock()
 playerimg = pygame.image.load('player.png')
 playerimg = pygame.transform.scale(playerimg,(100,100))
+ballimg = pygame.image.load('ball.png')
+ballimg = pygame.transform.scale(ballimg,(50,50))
+
 
 class GameObject():
     def __init__(self,x,y,img):
         self.rect = img.get_rect(center=((x,y)))
         self.speed = 3
+        self.speedx = self.speed
+        self.speedy = self.speed
 class Player(GameObject):
     def move(self):
         if self.rect.y < 0:
@@ -23,8 +28,24 @@ class Player(GameObject):
         if keylist[pygame.K_s]:
             self.rect.y += self.speed
         
+class Ball(GameObject):
+    def move(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.bottom <= 500:
+            self.speedy = -self.speedy
+        if self.rect.top >= 0:
+            self.speedy = -self.speedy
+        if self.rect.left <= 0:
+            self.speedx = -self.speedx
+            self.rect.center = (350,250)
+        if self.rect.right >= 700:
+            self.speedx = -self.speedx
+            self.rect.center = (350,250)
         
+
 player = Player(100,250,playerimg)
+ball = Ball(350,250,ballimg)
 
 canplay = True
 while canplay == True:
@@ -37,5 +58,7 @@ while canplay == True:
     window.fill('white')
     window.blit(playerimg,player.rect)
     player.move()
+    window.blit(ballimg,ball.rect)
+    ball.move()
     pygame.display.update()
 
